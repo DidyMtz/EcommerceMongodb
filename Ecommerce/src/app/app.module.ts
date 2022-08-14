a0acecd03df974204ace27ccffe1501a9a61e118
DidyMtz <102537566+DidyMtz@users.noreply.github.com>
2022-08-14 17:43:46 +0100

    installation ng new

---
+++ Ecommerce/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitComponent } from './operation/produit/produit.component';
+import { UserComponent } from './operation/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
